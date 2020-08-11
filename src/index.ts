e99741e5287cabbf9dde26a222de1bffec3bd318
Artem Diashkin <nemrosim1988@gmail.com>
2020-08-11 22:18:49 +0300

    start

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+
+
+class StartClass {
+    private foo: string;
+
+    constructor(foo: string) {
+        this.foo = foo;
+    }
+}
+
+const start = new StartClass('hello');
+console.log(start);
