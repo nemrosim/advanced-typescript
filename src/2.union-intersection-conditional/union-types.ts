74bc734ac587b5ca88420933f6171b5437aa6163
Artem Diashkin <nemrosim1988@gmail.com>
2020-08-13 00:09:55 +0300

    add union-types.ts

---
+++ src/2.union-intersection-conditional/union-types.ts
@@ -0,0 +1,69 @@
+"use strict";
+
+interface TextObjectProp {
+    size: string;
+}
+
+interface NumberObjectProps {
+    size: number,
+}
+
+const TextObject: TextObjectProp = {
+    size: '120px',
+}
+
+const NumberObject: NumberObjectProps = {
+    size: 120,
+}
+
+function setNewSize(obj: TextObjectProp | NumberObjectProps, obj2: TextObjectProp | NumberObjectProps) {
+    // @ts-ignore
+    let result = obj.size - obj2.size;
+    // console.log(result)
+}
+
+
+// === variant 1 ===
+function doSomething(value: string | number | boolean | undefined | null) {
+    console.log(`Do something. Value is: ${value}`)
+}
+
+doSomething(undefined);
+doSomething(null);
+
+// === variant 2 ===
+
+type DoSomethingElseProps = string | number | boolean | null
+
+function doSomethingElse(value?: DoSomethingElseProps) {
+    console.log(`Do something else. Value is: ${value}`)
+}
+
+doSomethingElse(undefined);
+doSomethingElse(null);
+
+// === variant 3 ===
+
+type DoSomeProps = {
+    value?: string | number | boolean;
+}
+
+function doSome({value}: DoSomeProps) {
+    console.log(`Do some. Value is: ${value}`)
+}
+
+doSome({value: undefined});
+doSome({value: null});
+
+
+
+
+
+
+
+
+
+
+
+
+
