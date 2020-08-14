1f064e859612247a4b90756f9cd452e56edf920f
Artem Diashkin <nemrosim1988@gmail.com>
2020-08-15 01:49:47 +0300

    Revert "add "doSome" and "doSomething" functions"
    This reverts commit 8cce3e64

--- example.ts
+++ example.ts
@@ -5,13 +5,4 @@ interface Some {
 class SomeImp implements Some {
     constructor(public some: string) {
     }
-
-    doSomething(){
-        console.log(this.some)
-    }
-
-
-    doSome() {
-        console.log('text');
-    }
 }
